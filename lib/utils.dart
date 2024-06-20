95a73d49f7b1283354af3804875ab3f77f91c16b
thanhtamit35 <thanhtamit35@gmail.com>
2024-06-20 15:31:05 +0700

    initial commit

---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> setUpFirebase() async {
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+}
